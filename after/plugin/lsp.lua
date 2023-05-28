9c036700cd7a42b708cc674eda5698337a59f08b
Emil Fresk <emil.fresk@gmail.com>
2023-05-28 08:00:17 +0200

    Shorthand to get to lsp config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -121,17 +121,6 @@ end
 -- },
 --})
 
-require('lspconfig').rust_analyzer.setup({
-    settings = {
-        ["rust-analyzer"] = {
-            checkOnSave = {
-                allTargets = false,
-                -- extraArgs = { "--bins" },
-            },
-        }
-    }
-})
-
 require("nlspsettings").setup({
     local_settings_dir = ".nvim",
     local_settings_root_markers_fallback = { '.git', 'Cargo.toml' },
---
+++ after/plugin/lspconfig.lua
@@ -0,0 +1,11 @@
+require('lspconfig').rust_analyzer.setup({
+    -- Uncomment for embedded work
+    --settings = {
+    --    ["rust-analyzer"] = {
+    --        checkOnSave = {
+    --            allTargets = false,
+    --            extraArgs = { "--bins" },
+    --        },
+    --    }
+    --}
+})
